02cda4610373c0e97e6c60923dcd049d237b467a
BeJoys8581 <beambeambenz81@gmail.com>
2023-10-06 16:33:32 +0700

    first commit

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <InputPage/>
+    <FunctionPage/>
+  </div>
+</template>
+
+<script>
+import InputPage from "src/pages/InputPage.vue"
+import FunctionPage from "src/pages/FunctionPage.vue"
+
+
+export default {
+  name: 'HtmlComponent',
+  components: {InputPage, FunctionPage},
+  data() {
+    return {
+      title: 'Chaiyut Suntharote'
+    };
+  }
+}
+</script>
---
+++ quasar-project/test/jest/__tests__/IndexPage.spec.js
@@ -0,0 +1,26 @@
+import IndexPage from 'quasar-project/src/pages/IndexPage.vue'
+import { shallowMount } from '@vue/test-utils'
+
+
+describe('IndexPage', () => {
+  it('should render correct contents', () => {
+    const wrapper = shallowMount(IndexPage)
+    let header = wrapper.find('.htmlClass h1')
+    expect(header.exists()).toBe(true)
+    expect(header.text())
+      .toBe('Chaiyut Suntharote')
+  })
+
+  it('check text content to be as defined in variable', () => {
+    const wrapper = shallowMount(IndexPage, {
+      data () {
+        return {
+          title: 'I love Vue.'
+        }
+      }
+    })
+    let header = wrapper.find('.htmlClass h1')
+    expect(header.text()).toBe('I love Vue.')
+  })
+
+  })
